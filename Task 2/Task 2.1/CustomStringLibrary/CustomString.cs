b07e6209e7efba47a331350a0b606b2556404908
Macr0pyre <iraengels2000@gmail.com>
2021-03-13 11:22:03 +0400

    Task 2.1.1 added

---
+++ Task 2/Task 2.1/CustomStringLibrary/CustomString.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStringLibrary
+{
+    public class CustomString : IComparable<CustomString>
+    {
+        private readonly char[] _line;
+
+        public CustomString()
+        {
+            _line = new char[] { };
+        }
+
+        public CustomString(char[] input)
+        {
+            _line = new char[input.Length];
+            input.CopyTo(_line, 0);
+        }
+
+        public CustomString(string str)
+        {
+            _line = str.ToCharArray();
+        }
+
+        public char[] Line
+        {
+            get { return _line; }
+        }
+
+        public static CustomString operator +(CustomString str1, CustomString str2)
+        {
+            char[] concated = new char[str1.Line.Length + str2.Line.Length];
+
+            str1.Line.CopyTo(concated, 0);
+            str2.Line.CopyTo(concated, str1.Line.Length);
+
+            return new CustomString(concated);
+        }
+
+        public static CustomString Concate(params CustomString[] list)
+        {
+            CustomString concated = new CustomString();
+            foreach (var item in list)
+            {
+                concated += item;
+            }
+
+            return concated;
+        }
+
+        public int CompareTo(CustomString other)
+        {
+            if (other.Line.Length == 0 && Line.Length == 0)
+            {
+                return 0;
+            }
+            else if (other.Line.Length == 0)
+            {
+                return 1;
+            }
+            else if (Line.Length == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                for (int i = 0; i < Math.Min(Line.Length, other.Line.Length); i++)
+                {
+                    if (Line[i] > other.Line[i])
+                        return 1;
+                    else if (Line[i] < other.Line[i])
+                        return -1;
+                }
+                return 0;
+            }
+        }
+
+        public bool Equals(CustomString other)
+        {
+            return CompareTo(other) == 0;
+        }
+
+        public int IndexOf(char symb)
+        {
+            for (int i = 0; i < Line.Length; i++)
+            {
+                if (Line[i] == symb)
+                    return i;
+            }
+            return -1;
+        }
+
+        public bool Contains(char symb)
+        {
+            return IndexOf(symb) != -1;
+        }
+
+        public char[] ToCharArray()
+        {
+            char[] outAr = new char[Line.Length];
+            Line.CopyTo(outAr, 0);
+            return outAr;
+        }
+
+        public override string ToString()
+        {
+            return new string(Line);
+        }
+
+        /// <summary>
+        /// Дополнительный метод, подсчитывающий количество повторений символа в строке.
+        /// </summary>
+        public int CountCharacters(char symb)
+        {
+            int cnt = 0;
+            foreach (char item in Line)
+            {
+                if (item == symb)
+                    cnt++;
+            }
+            return cnt;
+        }
+
+        /// <summary>
+        /// Дополнительный метод, создающий новую строку, повторяя известную строку определенное количество раз.
+        /// </summary>
+        public static CustomString ConcateOneLine(CustomString value, int count)
+        {
+            CustomString forOut = new CustomString();
+            for (int i = 0; i < count; i++)
+            {
+                forOut += value;
+            }
+            return forOut;
+        }
+
+        // *индексатор
+        //Нет сеттера, так как строка неизменяемая.
+        public char this[int index]
+        {
+            get
+            {
+                return Line[index];
+            }
+        }
+    }
+}
---
+++ Task 2/Task 2.1/Task 2 1 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using CustomStringLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_1_1
+{
+    class Program
+    {
+        //используется CustomStringLibrary (**)
+        static void Main(string[] args)
+        {
+            CustomString str1 = new CustomString(new char[] { 'a', 'b' });
+            CustomString str2 = new CustomString(new char[] { 'c', 'd' });
+            CustomString str0 = new CustomString();
+            CustomString strr = new CustomString("ab");
+            CustomString strNew = new CustomString(new char[] { 'a', 'b', 'c', 'b', 'a', '1' });
+
+            Console.WriteLine("Тестирование функциональности класса CustomString\n");
+
+            Console.WriteLine($"Конкатенация через метод: {CustomString.Concate(str1, str2, str0, strr)}");
+            Console.WriteLine($"Конкатенация через оператор: {str1 + str2 + str0 + strr}" + Environment.NewLine);
+
+            Console.WriteLine($"Сравнение через CompareTo строк {str1} и {str2}: {str1.CompareTo(str2)}");
+            Console.WriteLine($"Сравнение через Equals строк {str1} и {strr}: {str1.Equals(strr)}" + Environment.NewLine);
+
+            Console.WriteLine($"Поиск символа {'b'} в {str1} через IndexOf: {str1.IndexOf('b')} и Contains: {str1.Contains('b')}");
+            Console.WriteLine($"Поиск символа {'1'} в {strr} через IndexOf: {strr.IndexOf('1')} и Contains: {strr.Contains('1')}" + Environment.NewLine);
+
+            Console.WriteLine($"Конвертация строки {str1 + str2} в массив символов:");
+            char[] converted = (str1 + str2).ToCharArray();
+            foreach (var item in converted)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            //Если по заданию надо конвертацию из массива символов в свой тип, то есть просто конструктор для этого
+            string convStr = new CustomString(converted).ToString();
+            Console.WriteLine($"Конвертация этого массива символов в строку (String): {convStr}\n");
+
+            Console.WriteLine("Дополнительные функции:");
+            Console.WriteLine($"Подсчет количества повторений символа {'b'} в строке {strNew}: {strNew.CountCharacters('b')}");
+            Console.WriteLine($"Создание новой строки, повторяя строку {strNew} 3 раза: {CustomString.ConcateOneLine(strNew, 3)}\n");
+
+            Console.WriteLine($"Использование индексатора для взятия 1 элемента строки {strNew}: {strNew[1]}");
+        }
+    }
+}
